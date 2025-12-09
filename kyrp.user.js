293cec44f6312bdd8b396d62c3ba3f8456aa0cbe
trantung120213123 <142653654+trantung120213123@users.noreply.github.com>
2025-12-09 19:33:15 +0700

    Add initial user script for KrypBot

---
+++ kyrp.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         KrypBot
+// @namespace    http://tampermonkey.net/
+// @version      2025-1-2
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.chess.com/play/computer*
+// @match       https://www.chess.com/play/*
+// @match       https://www.chess.com/game/*
+// @match       https://play.chessgpt.vn/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// @require      https://github.com/trantung120213123/jk/raw/main/hjak
+// ==/UserScript==
